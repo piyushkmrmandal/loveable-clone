ca9c25794dad33c0974d9e6b638fa8a25fedc36d
Piyush Kumar Mandal <piyushkmrmandal@gmail.com>
2025-12-14 01:11:43 +0000

    Business Logic for ProjectController & ProjectMemberController with repository and Mapstruct conversions : reducing methods declarations in repository

--- src/main/java/com/learning/project/loveable_clone/repository/ProjectMemberRepository.java
+++ src/main/java/com/learning/project/loveable_clone/repository/ProjectMemberRepository.java
@@ -5,16 +5,9 @@ import com.learning.project.loveable_clone.entity.ProjectMemberId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Long> {
+public interface ProjectMemberRepository extends JpaRepository<ProjectMember, ProjectMemberId> {
 
     List<ProjectMember> findByIdProjectId(Long projectId);
-
-    boolean existsById(ProjectMemberId projectMemberId);
-
-    Optional<ProjectMember> findById(ProjectMemberId projectMemberId);
-
-    void deleteById(ProjectMemberId projectMemberId);
 }
